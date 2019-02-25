4ffabcb2c264bccfcff6a6e2c7d0999c10bfafb0
Kolby Kruger <kolbykruger@gmail.com>
2019-02-25 11:14:29 -0500

    updated script to use Open Sans

--- js/fonts.js
+++ js/fonts.js
@@ -7,7 +7,7 @@
         sessionStorage.fonts = true;
       },
       google: {
-        families: ['Montserrat:300,400,400i,500,600,700']
+        families: ['Open+Sans:300,400,400i,500,600,700']
       },
       timeout: 2000
     };
